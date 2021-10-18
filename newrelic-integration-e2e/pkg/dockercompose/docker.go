418aa8bdd34552687375a06f956e0b2ad2204fb0
Alvaro Cabanas <acabanas@newrelic.com>
2021-10-18 16:28:12 +0200

    docker-compose verbose
    Signed-off-by: Alvaro Cabanas <acabanas@newrelic.com>

--- newrelic-integration-e2e/pkg/dockercompose/docker.go
+++ newrelic-integration-e2e/pkg/dockercompose/docker.go
@@ -21,6 +21,7 @@ func Run(path string, container string, envVars map[string]string) error {
 		args = append(args, "-e", fmt.Sprintf("%s=%s", k, v))
 	}
 	args = append(args, "-d", container)
+	args = append(args, "-v")
 	cmd := exec.Command(dockerComposeBin, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
